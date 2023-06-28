46ced67e54f6c56c9d50fe0c6d0b3aea34bc88ca
Wanderson Moura <doorswanderson@hotmail.com>
2023-06-28 10:31:20 -0300

    adicao do ;

--- modulo_9/main.js
+++ modulo_9/main.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
             </div>       
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(5000)
+        $(novoItem).fadeIn(5000);
         $('#endereco-imagem-nova').val('')
     })
 })
